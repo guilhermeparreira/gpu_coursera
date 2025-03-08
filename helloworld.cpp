f6bdb26add580c293107436baa52fc7fd9a80ae5
Guilherme Parreira <guilhermeparreira.silva@gmail.com>
2025-03-08 11:31:31 -0300

    all changes

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+float f1 = 35e3;
+double d1 = 12E4;
+string x = "abcdef";
+
+int main()
+{   
+    cout << x.at(0);
+    cout << x[0];
+    return 1;
+}
